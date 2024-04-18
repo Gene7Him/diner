59c85797ab5b8949e8a1f5b166fb8a09b89d7755
gene7 <faison.eugene@student.greenriver.edu>
2024-04-18 12:35:53 -0400

    fat-free url routing

--- index.php
+++ index.php
@@ -20,5 +20,81 @@ $f3->route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+// Breakfast menu
+$f3->route('GET /menus/breakfast', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/breakfast-menu.html');
+});
+
+// Lunch menu
+$f3->route('GET /menus/lunch', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/lunch-menu.html');
+});
+
+// Dinner menu
+$f3->route('GET /menus/dinner', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/dinner-menu.html');
+});
+
+// Order Form Part I
+$f3->route('GET|POST /order1', function($f3) {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        //echo "<p>You got here using the POST method</p>";
+        //var_dump ($_POST);
+
+        // Get the data from the post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // If the data valid
+        // if (!empty($food) && !empty($meal)) {
+        if(true){
+            // Add the data to the session array
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+
+            // Send the user to the next form
+            $f3->reroute('order2');
+        }
+        else{
+            echo "<p>Validation Errors</p>";
+        }
+    }
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+// Order Form Part II
+$f3->route('GET /order2', function($f3) {
+    //echo '<h1>My Breakfast Menu</h1>';
+    var_dump ( $f3->get('SESSION') );
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
+});
+
+
+
+
+
 // Run Fat-Free
 $f3->run();
